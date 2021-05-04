56552ba258f69eac6af5df58dcafa5640a89c90f
katsuhisa91 <91katsuhisa@gmail.com>
2021-05-04 22:43:37 +0900

    株式会社からはじまる文字列を取得するサンプルプログラムを追加

---
+++ 2.2.1/sample_re.py
@@ -0,0 +1,10 @@
+import re
+
+sample_text = '株式会社アイデミー'
+sample_text2 = 'アイデミー株式会社'
+
+word = re.findall(r'^株式会社.*', sample_text)
+print(word)
+
+word = re.findall(r'^株式会社.*', sample_text2)
+print(word)
