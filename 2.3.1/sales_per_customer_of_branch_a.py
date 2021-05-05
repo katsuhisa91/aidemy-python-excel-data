fef1b16a55f355b44692aa75a2cf4d75853a343d
katsuhisa91 <91katsuhisa@gmail.com>
2021-05-05 17:58:12 +0900

    一行が長くなってしまったので、折り返す

--- 2.3.1/sales_per_customer_of_branch_a.py
+++ 2.3.1/sales_per_customer_of_branch_a.py
@@ -10,9 +10,11 @@ sales_per_customer = {}
 for i, cell in enumerate(branch_list):
     if cell.value == "A支店":
         if sheet.cell(row=i + 1, column=2).value in sales_per_customer.keys():
-            sales_per_customer[sheet.cell(row=i + 1, column=2).value] += sheet.cell(row=i + 1, column=8).value
+            sales_per_customer[sheet.cell(row=i + 1, column=2).value] += \
+                sheet.cell(row=i + 1, column=8).value
         else:
-            sales_per_customer[sheet.cell(row=i + 1, column=2).value] = sheet.cell(row=i + 1, column=8).value
+            sales_per_customer[sheet.cell(row=i + 1, column=2).value] = \
+                sheet.cell(row=i + 1, column=8).value
 
 # 売り上げの降順で並び替え
 for k, v in sorted(sales_per_customer.items(), key=lambda x: -x[1]):
--- 2.3.2/sales_per_quarter_of_branch_a.py
+++ 2.3.2/sales_per_quarter_of_branch_a.py
@@ -10,7 +10,8 @@ sales_per_quarter = [0, 0, 0, 0]
 for i, cell in enumerate(branch_list):
     if cell.value == "A支店":
         for j in range(4):
-            sales_per_quarter[j] += sheet.cell(row=i + 1, column=4 + j).value
+            sales_per_quarter[j] += \
+                sheet.cell(row=i + 1, column=4 + j).value
 
 print('第1四半期', sales_per_quarter[0])
 print('第2四半期', sales_per_quarter[1])
