064761b296b50880fca6c39923d36ffb9b4dec86
katsuhisa91 <91katsuhisa@gmail.com>
2021-05-05 22:23:42 +0900

    一行が長くなってしまったので、改行を挿入

--- 2.3.1/sales_per_customer_of_branch_a.py
+++ 2.3.1/sales_per_customer_of_branch_a.py
@@ -17,5 +17,6 @@ for i, cell in enumerate(branch_list):
                 sheet.cell(row=i + 1, column=8).value
 
 # 売り上げの降順で並び替え
-for k, v in sorted(sales_per_customer.items(), key=lambda x: -x[1]):
+for k, v in sorted(sales_per_customer.items(),
+                   key=lambda x: x[1], reverse=True):
     print(k, v)
