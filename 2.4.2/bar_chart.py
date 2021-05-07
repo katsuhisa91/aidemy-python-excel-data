741a6221a529c9836a109bffe3a278e6654f73f7
Katsuhisa Kitano <91katsuhisa@gmail.com>
2021-05-07 22:07:46 +0900

    2.4.2のサンプルプログラムと、関連するExcelファイルも修

--- 2.4.2/bar_chart.py
+++ 2.4.2/bar_chart.py
@@ -4,17 +4,14 @@ from openpyxl.chart import BarChart, Reference, Series
 wb = openpyxl.load_workbook('四半期別売上報告書.xlsx')
 ws = wb['支店別売上サマリ']
 
-ref_obj = Reference(ws, min_col=2, min_row=2, max_col=2, max_row=5)
-series_obj = Series(ref_obj, title='A支店の売上')
-
 chart = BarChart()
 chart.title = 'A支店の売上推移'
-chart.append(series_obj)
+
+values = Reference(ws, min_col=1, min_row=1, max_col=2, max_row=5)
+chart.add_data(values, titles_from_data=True)
 
 xvalues = Reference(ws, min_col=1, min_row=2, max_col=1, max_row=5)
 chart.set_categories(xvalues)
-chart.width = 20
-chart.x_axis.scaling.max = 10000000
 
 ws.add_chart(chart, 'A8')
 wb.save('四半期別売上報告書_グラフ追加済.xlsx')
